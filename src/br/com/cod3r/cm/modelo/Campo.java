a2a772494dd0d3b90a596fd20b274dcda15a4094
Fernando Shibata <shibata.421@gmail.com>
2020-03-05 13:12:31 -0300

    Atributos de campo

--- src/br/com/cod3r/cm/modelo/Campo.java
+++ src/br/com/cod3r/cm/modelo/Campo.java
@@ -1,5 +1,25 @@
 package br.com.cod3r.cm.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Campo {
 
+	private final int linha;
+	private final int coluna;
+	
+	private boolean aberto = false;
+	private boolean minado = false;
+	private boolean marcado = false;
+	
+	private List<Campo> vizinhos = new ArrayList<Campo>();
+	
+	Campo (int linha, int coluna){
+		this.linha = linha;
+		this.coluna = coluna;
+	}
+	
+	boolean adicionarVizinho (Campo vizinho) {
+		
+	}
 }
