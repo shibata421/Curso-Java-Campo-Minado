cd5566e072b450f40e123b1d8cd4692e4e9bd5c8
Fernando Shibata <shibata.421@gmail.com>
2020-03-06 13:13:43 -0300

    Aparecer bombas após perder

--- src/br/com/cod3r/cm/modelo/Tabuleiro.java
+++ src/br/com/cod3r/cm/modelo/Tabuleiro.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
+import br.com.cod3r.cm.excecao.ExplosaoException;
+
 public class Tabuleiro {
 
 	private int linhas;
@@ -23,10 +25,15 @@ public class Tabuleiro {
 	}
 	
 	public void abrir(int linha, int coluna) {
-		campos.parallelStream()
-			.filter(c -> c.getLinha() == linha && c.getColuna() == coluna)
-			.findFirst()
-			.ifPresent(c -> c.abrir());
+		try {
+			campos.parallelStream()
+				.filter(c -> c.getLinha() == linha && c.getColuna() == coluna)
+				.findFirst()
+				.ifPresent(c -> c.abrir());
+		} catch (ExplosaoException e) {
+			campos.forEach(c -> c.setAberto(true));
+			throw e;
+		}
 	}
 	
 	public void alterarMarcacao(int linha, int coluna) {
@@ -57,9 +64,9 @@ public class Tabuleiro {
 		Predicate<Campo> minado = c -> c.isMinado();
 		
 		do {
-			minasArmadas = campos.stream().filter(minado).count();
 			int aleatório = (int) (Math.random() * campos.size());
 			campos.get(aleatório).minar();
+			minasArmadas = campos.stream().filter(minado).count();
 		} while (minasArmadas < minas);
 	}	
 	
